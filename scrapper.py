4517135077fbb95a7d01233c93d64ab6879e1728
Demian <helloelliot666@protonmail.com>
2021-09-24 18:28:16 -0300

    refactoring again

--- scrapper.py
+++ scrapper.py
@@ -2,33 +2,55 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from geopy import distance
+import time
 
 options = Options()
 options.add_argument('--headless')
+options.add_argument('disable-infobars')
+options.add_argument('disable-extentions')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-application-cache')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+
+
 driver = webdriver.Firefox(options=options)
 
 API_KEY = "dba4558d-b961-4e34-bd1f-6b8837626a8d"
 MKAD = (55.751244, 37.618423)  # static point to be calculated
+
 test = (37.842762, 55.774558)  # any other place to take as parameter
 
 
-def generator_search(tag,  geocode):
+def generator_search(address):
     """ Searchs for a place by cordenates or address,
-        it takes a tag and prints out the information inside of it.
-        Need te function <find_tags()> to make i work. """
+        takes a tag the information inside a the information inside of it,. """
 
     url = "https://geocode-maps.yandex.ru/1.x/?apikey={}&geocode={}&lang=en-US".format(
-        API_KEY, geocode)
+        API_KEY, address)
     driver.get(url)
+    tags = ['Point', 'Address']
+    dictionary = {}
 
     try:
-        # search for the tag
-        locate = driver.find_element_by_tag_name(tag)
-        # return the information
-        yield locate.text
+        for tag in tags:
+            tag_text = driver.find_element_by_tag_name(
+                tag)  # search for the tag
 
-    except(NoSuchElementException):
-        yield print('Tag not found: {i}'.format(tag))
+            dictionary.update({tag: tag_text.text})
+
+            time.sleep(1)
+
+    except NoSuchElementException:
+        dictionary.update({tag: 'Not Found'})
+
+    finally:
+        driver.close()
+        return dictionary
+
+
+x = generator_search(test)
+print(x)
 
 
 def find_tags(address):
@@ -38,30 +60,36 @@ def find_tags(address):
 
     tags = ['Point', 'Address']
     dictionary = {}
-    for i in tags:
-        x = generator_search(i, address)
-        dictionary.update({i: next(x)})
+    try:
+        for i in tags:
+            x = generator_search(address)
+            dictionary.update({i: x})
+            time.sleep(2)
 
+    except ValueError:
+        return 'Sorry, looks like there is no results'
     return dictionary
 
 
-""" using the <find_tags[]> i can access the data insed of it"""
-
-# x = find_tags(test)
-# print(x['Point'])
-
-
 def calc_distance(local2):
-    """ calc distance in km, de default local1 will be mkad """
+    """ Calc the distance in km, de default local1 will be mkad, 
+        if the place is inside the mkad it doesent make the calc """
+
+    words = ['mkad', 'MKAD']
     take_point = find_tags(local2)
-    result = distance.distance(MKAD, take_point['Point'])
-    return result
+
+    try:
+        for word in words:
+            if word in take_point['Address']:
+                return f"Yor address is inside MKAD,there's no need to calculate the distance."
+            else:
+                return distance.distance(MKAD, take_point['Point'])
+    except ValueError:
+        return 'Sorry, looks like there is no results'
 
 
-x = calc_distance(test)
-print(x)
-
-
-def is_inside_mkad():
-    # look in the xml if is inside the mkad
-    is_in_mkad = driver.find_element_by_id('')
+"""
+a = find_tags('Moskow')
+c = calc_distance(a)
+print(next(c))
+"""
