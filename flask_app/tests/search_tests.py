cea319e0dc28df3542a7561f39780736d858bcbe
Demian <helloelliot666@protonmail.com>
2021-10-09 21:17:44 -0300

    Putting constants inside the test class

--- flask_app/tests/search_tests.py
+++ flask_app/tests/search_tests.py
@@ -1,21 +1,21 @@
 import unittest
 from flask_app.blueprints.distance_api.scrapper import Search
 
-soup_response = {'Point': '37.842762 55.774558',
-                 'Address': 'RURussia, Moscow, MKAD, 1st kilometre, inner sidecountryRussiaprovinceTsentralny federalny okrugprovinceMoscowlocalityMoscowstreetMKAD, 1st kilometre, inner side'}
-inside_mkad = "Your address is inside MKAD, there's no need to calculate the distance."
-coordinate = (37.842762, 55.774558)
-
 
 class TestSearch(unittest.TestCase):
+    INSIDE_MKAD = "Your address is inside MKAD, there's no need to calculate the distance."
+    COORDINATE = (37.842762, 55.774558)
+    SOUP_RESPONSE = {'Point': '37.842762 55.774558',
+                     'Address': 'RURussia, Moscow, MKAD, 1st kilometre, inner sidecountryRussiaprovinceTsentralny federalny okrugprovinceMoscowlocalityMoscowstreetMKAD, 1st kilometre, inner side'}
+
     def test_search_soup(self):
-        soup = Search.search_soup(self, coordinate)
-        self.assertEqual(soup, soup_response)
+        soup = Search.search_soup(self, TestSearch.COORDINATE)
+        self.assertEqual(soup, TestSearch.SOUP_RESPONSE)
 
     def test_calc_distance(self):
         search = Search()
-        distance = search.calc_distance(coordinate)
-        self.assertEqual(distance, inside_mkad)
+        distance = search.calc_distance(TestSearch.COORDINATE)
+        self.assertEqual(distance, TestSearch.INSIDE_MKAD)
 
 
 if __name__ == '__main__':
